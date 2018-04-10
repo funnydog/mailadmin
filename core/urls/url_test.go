3bc62e7d522db64cb371b35ab350e99f39848aa3
funnydog <funnydog@users.noreply.github.com>
2018-04-10 10:16:14 +0200

    Add a test suite for url.Manager

---
+++ core/urls/url_test.go
@@ -0,0 +1,57 @@
+package urls
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func TestURLManager(t *testing.T) {
+	router := httprouter.New()
+
+	manager := CreateManager(router)
+
+	myURL := URL{
+		Prefix:      "/",
+		Method:      "PUT",
+		HandlerFunc: func(http.ResponseWriter, *http.Request) {},
+		Name:        "friendly-name",
+	}
+
+	err := manager.Add(&myURL)
+	if err == nil {
+		t.Error("Expected error but got no error instead")
+		return
+	}
+
+	myURL.Method = "POST"
+	err = manager.Add(&myURL)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = manager.Add(&myURL)
+	if err == nil {
+		t.Error("Expected error but got no error instead")
+		return
+	}
+
+	myURL.Name = "other-name"
+	err = manager.Add(&myURL)
+	if err == nil {
+		t.Error("Expected error but got no error instead")
+		return
+	}
+
+	reverse, err := manager.Reverse("friendly-name", nil)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if reverse != myURL.Prefix {
+		t.Errorf("Expected '%s' but got '%s' instead", myURL.Prefix, reverse)
+	}
+}
